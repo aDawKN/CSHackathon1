bec73c80be5bfe4f731561591fe5a5636eec56a9
Darren Im <imdarren@Darrens-MacBook-Pro.local>
2023-10-19 18:32:23 -0700

    API code

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log('This is a popup!');
+async function fetchData(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'a048c41f45msh1746628f3b6ed2cp195beejsnc00ae02b3db1',
+            'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
