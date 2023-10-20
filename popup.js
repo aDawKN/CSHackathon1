25e524dc6871fbf3b551e3e2749074bf598dd2af
Darren Im <imdarren@Darrens-MacBook-Pro.local>
2023-10-19 18:51:56 -0700

    base code

--- popup.js
+++ popup.js
@@ -1,18 +1,22 @@
 async function fetchData(){
     const url = 'https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat';
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a048c41f45msh1746628f3b6ed2cp195beejsnc00ae02b3db1',
-            'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
-        }
+	    method: 'GET',
+	    headers: {
+		'X-RapidAPI-Key': 'a048c41f45msh1746628f3b6ed2cp195beejsnc00ae02b3db1',
+		'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+	    }
     };
-    
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
 
-    document.querySelector()
+    try {
+	    const response = await fetch(url, options);
+	    const result = await response.text();
+	    console.log(result);
+    } catch (error) {
+	    console.error(error);
+    }
+
+    // document.querySelector('div').innerHTML = result
 }
 
 fetchData();
